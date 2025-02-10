04895d8a9ec6ec39cdfee90fbd7fc25041f5efa1
MatheusVSan <mvgemage@gmail.com>
2025-02-10 11:27:59 -0300

    feat: foreach

---
+++ foreach/exemplo.01.php
@@ -0,0 +1,18 @@
+<?php 
+
+$meses = array (
+
+    "Janeiro", "Fevereiro", "Março",
+    "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro",
+    "Outubro", "Novembro", "Desembro"
+);
+
+foreach ($meses as $index => $mes){
+    
+    echo "indice:" .$index. "<br>";
+    
+    echo "O mes é:" .$mes. "<br><br>";
+}
+
+?>
