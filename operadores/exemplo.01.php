e6f0cfbbd774c066e92da6f8cb79dae9ed648f04
MatheusVSan <mvgemage@gmail.com>
2025-02-10 12:37:28 -0300

    fix: modificado

---
+++ operadores/exemplo.01.php
@@ -0,0 +1,12 @@
+<?php 
+
+$nome = "Hcode" ;
+
+echo $nome . " mais alguma coisa";
+
+$nome .= " treinamento";
+
+echo $nome;
+
+
+?>
---
+++ operadores/exemplo.03.php
@@ -0,0 +1,31 @@
+<?php
+
+$a = 10;
+$b = 2;
+
+/*Soma*/
+echo $a + $b;
+ echo "<br>";
+
+ /*Menos*/
+echo $a - $b;
+ echo "<br>";
+
+ /*Vezes*/
+echo $a * $b;
+ echo "<br>";
+
+ /*Divisçao*/
+echo $a / $b;
+ echo "<br>";
+
+/*Porcentagem*/
+echo $a % $b;
+ echo "<br>";
+
+/*Exponenciação*/
+echo $a ** $b;
+ echo "<br>";
+ 
+
+?>
