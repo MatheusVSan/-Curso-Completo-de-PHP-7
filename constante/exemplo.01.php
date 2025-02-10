803d0cd62b98c1253548a0a125c024083e5992ea
MatheusVSan <mvgemage@gmail.com>
2025-02-10 15:57:21 -0300

    feat: constates

---
+++ constante/exemplo.01.php
@@ -0,0 +1,9 @@
+<?php
+
+/*Aprendendo a usar constantes sempre para organização MAIUSCULAS*/
+
+define ("SERVIDOR","127.0.0.1");
+
+echo SERVIDOR;
+
+?>
---
+++ constante/exemplo.03.php
@@ -0,0 +1,10 @@
+<?php   
+
+/*Aprendendo CONSTANTES definidas...*/
+
+echo PHP_VERSION;
+
+echo "<br>";
+
+echo DIRECTORY_SEPARATOR;
+?>
